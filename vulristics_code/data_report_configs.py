cd62a971b6a772042fcc0e751686f2697a52587b
Dmitriy K <delta-inc@live.ru>
2023-12-06 01:08:21 +0300

    report styles

--- vulristics_code/data_report_configs.py
+++ vulristics_code/data_report_configs.py
@@ -1,18 +1,43 @@
 patch_tuesday_report_configs = {
+    # Company
+    'with_comments_local_img_company': {
+        'file_name_suffix': "report_with_comments_local_img",
+        'vuln_icons_source': "icons_company",
+        'ignore_comments': False
+    },
+    # 'no_comments_local_img_company': {
+    #     'file_name_suffix': "no_comments_local_img",
+    #     'vuln_icons_source': "icons_company",
+    #     'ignore_comments': True
+    # },
+
+    # Personal
+    # 'with_comments_local_img_personal': {
+    #     'file_name_suffix': "report_with_comments_local_img",
+    #     'vuln_icons_source': "icons_personal",
+    #     'ignore_comments': False
+    # },
+    # 'no_comments_local_img_personal': {
+    #     'file_name_suffix': "no_comments_local_img",
+    #     'vuln_icons_source': "icons_personal",
+    #     'ignore_comments': True
+    # },
+
+    # Stock
     # 'with_comments_local_img': {
     #     'file_name_suffix': "report_with_comments_local_img",
     #     'vuln_icons_source': "icons",
     #     'ignore_comments': False
     # },
-    'with_comments_ext_img': {
-        'file_name_suffix': "report_with_comments_ext_img",
-        'vuln_icons_source': "http://www.avleonov.com/vulnicons",
-        'ignore_comments': False
-    },
     # 'no_comments_local_img': {
     #     'file_name_suffix': "no_comments_local_img",
     #     'vuln_icons_source': "icons",
     #     'ignore_comments': True
+    # },
+    # 'with_comments_ext_img': {
+    #     'file_name_suffix': "report_with_comments_ext_img",
+    #     'vuln_icons_source': "http://www.avleonov.com/vulnicons",
+    #     'ignore_comments': False
     # },
     # 'no_comments_ext_img': {
     #     'file_name_suffix': "report_no_comments_ext_img",
